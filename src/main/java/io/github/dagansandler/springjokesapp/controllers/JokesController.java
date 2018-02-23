97cac747eff822d0814ff6261f85f78b4aaa6ef9
Dagan Sandler <dagansandler@gmail.com>
2018-02-23 13:51:52 +0200

    tasks 5-7
    Map context root (“/“, “”) to Jokes view
    Add Joke text to ‘joke’ property of Model
    Return view name of ‘chucknorris’

--- src/main/java/io/github/dagansandler/springjokesapp/controllers/JokesController.java
+++ src/main/java/io/github/dagansandler/springjokesapp/controllers/JokesController.java
@@ -1,8 +1,24 @@
 package io.github.dagansandler.springjokesapp.controllers;
 
+import io.github.dagansandler.springjokesapp.services.JokesService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JokesController {
+
+private JokesService jokesService;
+
+    public JokesController(JokesService jokesService) {
+        this.jokesService = jokesService;
+    }
+
+    @RequestMapping({"/",""})
+    public String getJokes(Model model) {
+        model.addAttribute("joke", jokesService.getJoke());
+
+        return "chucknorris";
+    }
 
 }
